e2c48a168c511ae68338658f78964dd54057b77f
HoangAnhNguyen0710 <anhnh1@hblab.vn>
2023-02-12 11:56:23 +0700

    fix conflict

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
+    @breakingNews = Article.limit(4).order(created_at: :desc)
+    @category = Category.all
+    @articlesByCategory = []
+    @category.each do |category|
+      @articlesByCategory[category.id] = Article.where(category_id: category.id)
+    end
   end
 
   def category
