c8386af8f02871b2d1e6f88cc48c5fc4d369e9e3
HoangAnhNguyen0710 <anhnh1@hblab.vn>
2023-02-14 11:19:11 +0700

    static pages: get flash news

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
     @lastest_news = Article.order("created_at DESC").limit(4)
+    @flash_news = Article.joins(:category).order("articles.created_at DESC").limit(3).group("categories.id")
   end
 end
